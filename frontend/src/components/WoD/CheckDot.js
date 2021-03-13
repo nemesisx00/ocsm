0647f4c273317e24be32157af0b850180d8b31fe
Peter Lunneberg <plunn.dev@gmail.com>
2021-03-13 16:26:21 -0500

    Updated CheckDot to follow React's unidirectional data flow; its 'checked' state will always be controlled from one of its parent components so it doesn't need a local state.

--- frontend/src/components/WoD/CheckDot.js
+++ frontend/src/components/WoD/CheckDot.js
@@ -5,31 +5,16 @@ const CheckedClass = 'checked'
 
 class CheckDot extends React.Component
 {
-	constructor(props)
-	{
-		super(props)
-		this.state = {
-			checked: props.checked
-		}
-	}
-	
 	render()
 	{
 		return (
-			<div className={'checkDot' + (this.state.checked ? ` ${CheckedClass}` : '')}
-					onClick={() => this.toggleCheckedState()}>
+			<div className={`checkDot ${this.props.className} ${(this.props.checked ? ` ${CheckedClass}` : '')}`}
+					onClick={() => this.props.onClick()}>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
 					<circle cx="7" cy="7" r="7" />
 				</svg>
 			</div>
 		)
-	}
-	
-	toggleCheckedState()
-	{
-		this.setState((state) => {
-			return { checked: !state.checked }
-		})
 	}
 }
 
