fefe54404a5153018d69148b242922371dd774f0
Peter Lunneberg <plunn.dev@gmail.com>
2021-03-13 13:59:53 -0500

    Updated MainMenu Component: - Swapped the 'active' state to Hide, as it fits standard web functionality and conventions better. - Added DevSafeTauriInvoke.js to make Tauri's invoke safe when you are only running the frontend during development; Just wraps the call in a try-catch and logs a simple notification on error. - Added generic mouse event handlers for enter/leave to handle automatically closing submenus.

--- frontend/src/components/core/MainMenu.js
+++ frontend/src/components/core/MainMenu.js
@@ -1,10 +1,14 @@
 import './MainMenu.css'
 import React from 'react'
-import { invoke } from 'tauri/api/tauri'
+import { devSafeInvoke } from './DevSafeTauriInvoke'
+const invoke = devSafeInvoke
+
+const subMenuCloseTimeoutTime = 1500
+let subMenuCloseTimeout = null
 
 const MenuStates = Object.freeze({
-	Hide: '',
-	Show: 'show'
+	Hide: 'hide',
+	Show: ''
 })
 
 export default class MainMenu extends React.Component
@@ -24,7 +28,7 @@ export default class MainMenu extends React.Component
 		return (
 			<div className="mainMenu">
 				<div className="menu file" onClick={(ev) => this.clickHandler_menu(ev)}>File</div>
-				<div className={`subMenu file ${this.state.subMenus.file}`}>
+				<div className={`subMenu file ${this.state.subMenus.file}`} onMouseEnter={(ev) => this.genericMouseEnterHandler(ev)} onMouseLeave={(ev) => this.genericMouseLeaveHandler(ev)}>
 					<div className="menuItem" onClick={(ev) => this.clickHandler_file_exit(ev)}>Exit</div>
 				</div>
 			</div>
@@ -33,24 +37,38 @@ export default class MainMenu extends React.Component
 	
 	clickHandler_menu(ev)
 	{
-		let menuName = ev.target.innerText.toLowerCase()
-		let newState = { subMenus: {} }
-		
-		newState.subMenus[menuName] = this.state.subMenus[menuName] === MenuStates.Hide ? MenuStates.Show : MenuStates.Hide
-		
-		this.setState(() => {
-			return newState
-		})
+		this.switchSubmenuState(ev.target.classList[1].toLowerCase())
 	}
 	
-	clickHandler_file_exit(ev)
+	clickHandler_file_exit()
 	{
 		this.setState(() => {
 			return { subMenus: { file: MenuStates.Hide } }
 		})
 		
-		invoke({
-			cmd: 'exitApp'
-		})
+		invoke({ cmd: 'exitApp' })
+	}
+	
+	genericMouseEnterHandler()
+	{
+		if(subMenuCloseTimeout)
+			clearTimeout(subMenuCloseTimeout)
+	}
+	
+	genericMouseLeaveHandler(ev)
+	{
+		if(subMenuCloseTimeout)
+			clearTimeout(subMenuCloseTimeout)
+		
+		subMenuCloseTimeout = setTimeout(() => {
+			this.switchSubmenuState(ev.target.classList[1].toLowerCase())
+		}, subMenuCloseTimeoutTime)
+	}
+	
+	switchSubmenuState(menuName)
+	{
+		let newState = { subMenus: {} }
+		newState.subMenus[menuName] = this.state.subMenus[menuName] === MenuStates.Hide ? MenuStates.Show : MenuStates.Hide
+		this.setState(() => { return newState })
 	}
 }
