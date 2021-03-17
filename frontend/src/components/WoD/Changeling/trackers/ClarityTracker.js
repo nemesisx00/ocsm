df72cfd1a13cc15461f4d881f00e31fa0fe6e210
Peter Lunneberg <plunn.dev@gmail.com>
2021-03-17 01:10:45 -0400

    Added a Clarity tracker component.

---
+++ frontend/src/components/WoD/Changeling/trackers/ClarityTracker.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import CheckDot from '../../CheckDot'
+import './ClarityTracker.css'
+
+export default class ClarityTracker extends React.Component
+{
+	render()
+	{
+		return (
+			<div className="clarityTracker">
+				<p className="label">Clarity</p>
+				<div className="clarity">
+					<CheckDot className="one" checked={this.props.clarity > 0} onClick={() => this.props.changeHandler(1)} />
+					<CheckDot className="two" checked={this.props.clarity > 1} onClick={() => this.props.changeHandler(2)} />
+					<CheckDot className="three" checked={this.props.clarity > 2} onClick={() => this.props.changeHandler(3)} />
+					<CheckDot className="four" checked={this.props.clarity > 3} onClick={() => this.props.changeHandler(4)} />
+					<CheckDot className="five" checked={this.props.clarity > 4} onClick={() => this.props.changeHandler(5)} />
+					<CheckDot className="six" checked={this.props.clarity > 5} onClick={() => this.props.changeHandler(6)} />
+					<CheckDot className="seven" checked={this.props.clarity > 6} onClick={() => this.props.changeHandler(7)} />
+					<CheckDot className="eight" checked={this.props.clarity > 7} onClick={() => this.props.changeHandler(8)} />
+					<CheckDot className="nine" checked={this.props.clarity > 8} onClick={() => this.props.changeHandler(9)} />
+					<CheckDot className="ten" checked={this.props.clarity > 9} onClick={() => this.props.changeHandler(10)} />
+				</div>
+			</div>
+		)
+	}
+}
