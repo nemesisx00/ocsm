a4b358d7f6f62207ba42d538a307e55702cba9ce
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-02-01 18:40:59 -0500

    Added script to compile Stylus into CSS
    For now, I'm just adding a compile step to do before React's build command as a brute force means of introducing Stylus into the project. I intend to leverage Stylus' full capabilities to greatly simply this process in the future.

---
+++ frontend/compileStylus.js
@@ -0,0 +1,40 @@
+const dree = require('dree')
+const { exec } = require('child_process')
+
+const generateCommand = (path) => `stylus --compress stylus/components/${path} --out src/components/${path}`
+
+const tree = dree.scan('./stylus/components', {
+	normalize: true,
+	depth: 10,
+	descendants: true,
+	descendantsIgnoreDirectories: true,
+	extensions: [ 'styl' ]
+})
+
+const fillCommandList = (obj, commandList) => {
+	for(let child of obj.children)
+	{
+		if(child.type === 'directory')
+		{
+			let command = generateCommand(child.relativePath)
+			commandList.push(command)
+		}
+		
+		if(child.children && child.children.length > 0)
+			fillCommandList(child, commandList)
+	}
+}
+
+let commands = [ `stylus --compress stylus/core --out src/` ]
+fillCommandList(tree, commands)
+console.log(commands)
+
+for(let command of commands)
+{
+	exec(command, (err, stdout, stderr) => {
+		if(err)
+			console.error(err)
+		else
+			console.log(stdout)
+	})
+}
