920d7bab63cf1b0ccb997d8fe70b438c84cb414d
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-03-28 22:57:18 -0400

    Update main.rs
    Added function to generate a native Main Menu. Not entirely sure how to make the menu items actually do things in Dioxus just yet, so I'll stick with my custom built one for the moment.

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,54 @@ pub(crate) mod core;
 pub(crate) mod cod;
 
 use dioxus::desktop::{
-	tao::dpi::LogicalSize,
+	tao::{
+		dpi::LogicalSize,
+		menu::{
+			MenuBar,
+			MenuItemAttributes,
+		},
+	},
 	launch_cfg
 };
 use crate::app::App;
+
+pub const WindowTitle: &str = "Open Character Sheet Manager";
 
 fn main()
 {
     launch_cfg(App, |cfg|
 	{
-		cfg.with_window(|w|
-		{
-			w.with_title("Open Character Sheet Manager")
-				.with_resizable(true)
-				.with_inner_size(LogicalSize::new(900.0, 800.0))
-		})
+		cfg
+			//.with_disable_context_menu(true)
+			.with_window(|w|
+			{
+				w.with_title(WindowTitle)
+					//.with_menu(generateMainMenu())
+					.with_resizable(true)
+					.with_inner_size(LogicalSize::new(900.0, 800.0))
+			})
 	});
+}
+
+#[allow(dead_code)]
+fn generateMainMenu() -> MenuBar
+{
+	let mut fileMenu = MenuBar::new();
+	fileMenu.add_item(MenuItemAttributes::new("New"));
+	fileMenu.add_item(MenuItemAttributes::new("Open"));
+	fileMenu.add_item(MenuItemAttributes::new("Save"));
+	fileMenu.add_item(MenuItemAttributes::new("Exit"));
+	
+	let mut gameSystems = MenuBar::new();
+	gameSystems.add_item(MenuItemAttributes::new("Changeling: The Lost - Second Edition"));
+	gameSystems.add_item(MenuItemAttributes::new("Mage: The Awakening - Second Edition"));
+	gameSystems.add_item(MenuItemAttributes::new("Vampire: The Requiem - Second Edition"));
+	gameSystems.add_item(MenuItemAttributes::new("Dungeons & Dragons 5th Edition"));
+	gameSystems.add_item(MenuItemAttributes::new("Vampire: The Masquerade 5th edition"));
+	
+	let mut mainMenu = MenuBar::new();
+	mainMenu.add_submenu("File", true, fileMenu);
+	mainMenu.add_submenu("Game Systems", true, gameSystems);
+	
+	return mainMenu;
 }
