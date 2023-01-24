1a1c87b10a82518feec8aba2de727c324f78d336
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-01-24 16:32:14 -0500

    Implementing ItemWeapon
    Added Enums.NET via NuGet for allowing more complex uses of enumerations.
    
    Created Range to describe the short and long range values of a weapon.
    Created DamageDie to directly associate a DamageDie.DamageType with a Die.
    Created ItemWeapon to describe all pertinent information about a D&D 5E weapon.

---
+++ scripts/data/dnd/fifth/DamageDie.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace OCSM.DnD.Fifth
+{
+	public class DamageDie : Die, IComparable<DamageDie>, IEquatable<DamageDie>
+	{
+		public enum DamageType
+		{
+			None = 0,
+			Bludgeoning,
+			Piercing,
+			Slashing,
+		}
+		
+		public DamageType Type { get; set; }
+		
+		public DamageDie() : base()
+		{
+			Type = DamageType.None;
+		}
+		
+		public int CompareTo(DamageDie other)
+		{
+			var ret = 0;
+			if(other is DamageDie)
+			{
+				ret = base.CompareTo(other);
+				if(ret.Equals(0))
+					ret = Type.CompareTo(other.Type);
+			}
+			return ret;
+		}
+		
+		public bool Equals(DamageDie other)
+		{
+			return base.Equals(other)
+				&& Type.Equals(other.Type);
+		}
+	}
+}
---
+++ scripts/data/dnd/fifth/inventory/ItemWeapon.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using EnumsNET;
+
+namespace OCSM.DnD.Fifth.Inventory
+{
+	public class ItemWeapon : Item, IComparable<ItemWeapon>, IEquatable<ItemWeapon>
+	{
+		public enum WeaponType
+		{
+			None = 0,
+			Simple = SimpleMelee | SimpleRanged,
+			SimpleMelee = 1,
+			SimpleRanged = 2,
+			Martial = MartialMelee | MartialRanged,
+			MartialMelee = 4,
+			MartialRanged = 8,
+			Improvised = 16,
+		}
+		
+		public enum WeaponProperties
+		{
+			None = 0,
+			Ammunition,
+			Finesse,
+			Heavy,
+			Light,
+			Loading,
+			Range,
+			Reach,
+			Silvered,
+			Special,
+			Thrown,
+			TwoHanded,
+			Versatile,
+		}
+		
+		public static string FormatProperties(List<WeaponProperties> properties)
+		{
+			var str = "";
+			
+			properties.Sort();
+			foreach(var prop in properties)
+			{
+				if(str.Length > 0)
+					str += ", ";
+				str += Enums.GetName(prop);
+			}
+			
+			return str;
+		}
+		
+		public Dictionary<DamageDie, int> DamageDice { get; set; }
+		public List<WeaponProperties> Properties { get; set; }
+		public Range Range { get; set; }
+		public WeaponType Type { get; set; }
+		
+		public ItemWeapon() : base()
+		{
+			DamageDice = new Dictionary<DamageDie, int>();
+			Equippable = true;
+			Properties = new List<WeaponProperties>();
+			Range = Range.Melee;
+			Type = WeaponType.None;
+		}
+		
+		public int CompareTo(ItemWeapon other)
+		{
+			var ret = Type.CompareTo(other.Type);
+			if(other is ItemWeapon)
+			{
+				if(ret.Equals(0))
+					ret = base.CompareTo(other);
+				if(ret.Equals(0))
+					ret = Range.CompareTo(other.Range);
+				if(ret.Equals(0))
+					ret = DamageDice.Keys.Count.CompareTo(other.DamageDice.Keys.Count);
+				if(ret.Equals(0))
+					ret = Properties.Count.CompareTo(other.Properties.Count);
+				if(ret.Equals(0))
+					ret = ItemWeapon.FormatProperties(Properties).CompareTo(ItemWeapon.FormatProperties(other.Properties));
+			}
+			return ret;
+		}
+		
+		public bool Equals(ItemWeapon item)
+		{
+			return base.Equals(item)
+				&& Properties.Equals(item.Properties)
+				&& Type.Equals(item.Type);
+		}
+	}
+}
