8ede2689a15d2301577ec1b7a02117e69176e7ee
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-01-24 23:21:15 -0500

    Create ItemContainer.cs
    Created ItemContainer to be an item that can contain a list of other items.

---
+++ scripts/data/dnd/fifth/inventory/ItemContainer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace OCSM.DnD.Fifth.Inventory
+{
+	public class ItemContainer : ItemEquippable, IComparable<ItemContainer>, IEquatable<ItemContainer>
+	{
+		public List<Item> Items { get; set; }
+		
+		public ItemContainer() : base()
+		{
+			Items = new List<Item>();
+		}
+		
+		public int CompareTo(ItemContainer other)
+		{
+			var ret = base.CompareTo(other);
+			if(other is ItemContainer)
+			{
+				ret = Items.Count.CompareTo(other.Items.Count);
+			}
+			return ret;
+		}
+		
+		public bool Equals(ItemContainer other)
+		{
+			return base.Equals(other)
+				&& Items.Equals(other.Items);
+		}
+		
+		public double totalWeight()
+		{
+			var val = Weight;
+			Items.ForEach(i => val += i.Weight);
+			return val;
+		}
+	}
+}
