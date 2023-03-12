609ed6aabebbabda803365e37a8fd9d9aec01fe8
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-03-11 21:24:08 -0500

    Created xUnit Project
    Created OCSM.Tests project to house xUnit tests.

---
+++ OCSM.Tests/util/ExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿
+namespace OCSM.Tests.util
+{
+	public class ExtensionsTest
+	{
+		enum MyEnum
+		{
+			[Label("Hi")]
+			Hi,
+			[Label("There")]
+			There,
+		}
+		
+		[Fact]
+		public void LabelTest()
+		{
+			Assert.Equal("Hi", MyEnum.Hi.GetLabel());
+			Assert.Equal("There", MyEnum.There.GetLabel());
+		}
+	}
+}
---
+++ OCSM.Tests/util/LogicTests.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OCSM.Tests.util
+{
+	public class LogicTests
+	{
+		public static IEnumerable<object[]> GetTrueData()
+			=> new object[][]
+			{
+				new object[] { "Hi", "Hi" },
+				new object[] { 5, 5 },
+				new object[] { null, null },
+				new object[] { new Pair() { Key = "fun", Value = "times" } , new Pair() { Key = "fun", Value = "times" } },
+			};
+		
+		public static IEnumerable<object[]> GetFalseData()
+			=> new object[][]
+			{
+				new object[] { "Hi", "BYe" },
+				new object[] { 5, 10 },
+				new object[] { null, true },
+				new object[] { new Pair() { Key = "fun", Value = "times" } , new Pair() { Key = "fail", Value = "times" } },
+				new object[] { new Pair() { Key = "fun", Value = "times" } , new Pair() { Key = "fun", Value = "again" } },
+			};
+		
+		[Theory]
+		[MemberData(nameof(GetTrueData))]
+		public void TestEqual_True<T>(T one, T two)
+		{
+			Assert.True(Logic.AreEqualOrNull(one, two));
+		}
+		
+		[Theory]
+		[MemberData(nameof(GetFalseData))]
+		public void TestEqual_False<T>(T one, T two)
+		{
+			Assert.False(Logic.AreEqualOrNull(one, two));
+		}
+	}
+}
