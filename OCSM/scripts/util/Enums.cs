526e0d579bf7902ce16eacce8ece1cb27959ded9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-03-15 14:56:18 -0400

    Update Enums.cs
    Make Enums a static class.
    Simplify the logic in AreEqualOrNull.

--- OCSM/scripts/util/Enums.cs
+++ OCSM/scripts/util/Enums.cs
@@ -6,7 +6,7 @@ namespace Ocsm;
 /// <summary>
 /// Class providing convenience methods for evaluating logic.
 /// </summary>
-public class Enums
+public static class Enums
 {
 	/// <summary>
 	/// Convenience method to get an enumeration value based on its name string.
@@ -53,8 +53,8 @@ public class Enums
 	public static bool AreEqualOrNull<T>(T o1, T o2)
 	{
 		return (
-			(o1 is T && o1.Equals(o2))
-			|| (!(o1 is T) && !(o2 is T))
+			(o1 is not null && o1.Equals(o2))
+			|| (o1 is null && o2 is null)
 		);
 	}
 }
