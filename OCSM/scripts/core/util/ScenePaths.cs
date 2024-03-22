f93593c9d6da51061b6410e95ca0e78c12792d17
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-03-22 16:05:16 -0400

    Update ScenePaths.cs
    Order the public members above the private members.

--- OCSM/scripts/core/util/ScenePaths.cs
+++ OCSM/scripts/core/util/ScenePaths.cs
@@ -2,8 +2,6 @@ namespace Ocsm;
 
 public static class ScenePaths
 {
-	private const string Prefix = "res://scenes";
-	
 	public const string AboutGodot = $"{Prefix}/AboutGodot.tscn";
 	public const string AboutOcsm = $"{Prefix}/AboutOcsm.tscn";
 	public const string ConfirmQuit = $"{Prefix}/ConfirmQuit.tscn";
@@ -15,36 +13,38 @@ public static class ScenePaths
 	public const string StatefulButton = $"{Prefix}/StatefulButton.tscn";
 	public const string ToggleButton = $"{Prefix}/ToggleButton.tscn";
 	
+	private const string Prefix = "res://scenes";
+	
 	public static class Meta
 	{
-		private const string PrefixMeta = $"{Prefix}/meta";
-		
 		public const string ConfirmDeleteEntry = $"{PrefixMeta}/ConfirmDeleteEntry.tscn";
+		
+		private const string PrefixMeta = $"{Prefix}/meta";
 	}
 	
 	public static class Cofd
 	{
-		private const string PrefixCofd = $"{Prefix}/cofd";
-		
 		public const string ItemDots = $"{PrefixCofd}/ItemDots.tscn";
 		public const string Specialty = $"{PrefixCofd}/Specialty.tscn";
 		
+		private const string PrefixCofd = $"{Prefix}/cofd";
+		
 		public static class Mortal
 		{
-			private const string PrefixMortal = $"{PrefixCofd}/mortal";
-			
 			public const string NewSheetName = "New Mortal";
 			public const string Sheet = $"{PrefixMortal}/Mortal.tscn";
+			
+			private const string PrefixMortal = $"{PrefixCofd}/mortal";
 		}
 		
 		public static class Changeling
 		{
-			private const string PrefixCtl = $"{PrefixCofd}/ctl";
-			
 			public const string ContractNode = $"{PrefixCtl}/ContractNode.tscn";
 			public const string NewSheetName = "New Changeling";
 			public const string SeemingBenefit = $"{PrefixCtl}/SeemingBenefit.tscn";
 			public const string Sheet = $"{PrefixCtl}/ChangelingTheLost.tscn";
+			
+			private const string PrefixCtl = $"{PrefixCofd}/ctl";
 			
 			public static class Meta
 			{
@@ -59,8 +59,6 @@ public static class ScenePaths
 		
 		public static class Fifth
 		{
-			private const string PrefixDnd5e = $"{PrefixDnd}/fifth";
-			
 			public const string Skill = $"{PrefixDnd5e}/Skill.tscn";
 			public const string Sheet = $"{PrefixDnd5e}/Fifth.tscn";
 			public const string NewSheetName = "New Adventurer";
@@ -68,14 +66,16 @@ public static class ScenePaths
 			public const string FeatureSection = $"{PrefixDnd5e}/FeatureSection.tscn";
 			public const string InventoryItem = $"{PrefixDnd5e}/InventoryItem.tscn";
 			
+			private const string PrefixDnd5e = $"{PrefixDnd}/fifth";
+			
 			public static class Meta
 			{
-				private const string PrefixDnd5eMeta = $"{PrefixDnd5e}/meta";
-				
 				public const string AddEditMetadata = $"{PrefixDnd5eMeta}/DndFifthAddEditMetadata.tscn";
 				public const string FeatureSectionEntry = $"{PrefixDnd5eMeta}/FeatureSectionEntry.tscn";
 				public const string FeatureEntry = $"{PrefixDnd5eMeta}/FeatureEntry.tscn";
 				public const string NumericBonusEdit = $"{PrefixDnd5eMeta}/NumericBonusEdit.tscn";
+				
+				private const string PrefixDnd5eMeta = $"{PrefixDnd5e}/meta";
 			}
 		}
 	}
