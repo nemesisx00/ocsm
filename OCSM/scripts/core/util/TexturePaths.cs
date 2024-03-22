f423dde2be0e3cf2e6cab56d80e120335d6aff9b
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-03-22 16:01:59 -0400

    Update TexturePaths.cs

--- OCSM/scripts/core/util/TexturePaths.cs
+++ OCSM/scripts/core/util/TexturePaths.cs
@@ -2,13 +2,15 @@ namespace Ocsm;
 
 public static class TexturePaths
 {
-	public const string FullTransparent = "res://assets/textures/box-transparent-16.png";
-	public const string TrackBoxBorder = "res://assets/textures/box-border-16.png";
-	public const string TrackBox1 = "res://assets/textures/slash-one.png";
-	public const string TrackBox2 = "res://assets/textures/slash-two.png";
-	public const string TrackBox3 = "res://assets/textures/slash-three.png";
-	public const string TrackCircle = "res://assets/textures/circle-empty.png";
-	public const string TrackCircleFill = "res://assets/textures/circle-fill.png";
-	public const string TrackCircleHalf = "res://assets/textures/circle-fill-half.png";
-	public const string TrackCircleRed = "res://assets/textures/circle-fill-red.png";
+	private const string Prefix = "res://assets/textures";
+	
+	public const string FullTransparent = $"{Prefix}/box-transparent-16.png";
+	public const string TrackBoxBorder = $"{Prefix}/box-border-16.png";
+	public const string TrackBox1 = $"{Prefix}/slash-one.png";
+	public const string TrackBox2 = $"{Prefix}/slash-two.png";
+	public const string TrackBox3 = $"{Prefix}/slash-three.png";
+	public const string TrackCircle = $"{Prefix}/circle-empty.png";
+	public const string TrackCircleFill = $"{Prefix}/circle-fill.png";
+	public const string TrackCircleHalf = $"{Prefix}/circle-fill-half.png";
+	public const string TrackCircleRed = $"{Prefix}/circle-fill-red.png";
 }
