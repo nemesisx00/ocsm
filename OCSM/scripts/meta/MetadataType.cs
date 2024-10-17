950a2fa34c95071f2fbe15f27d242abc61175eac
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-10-17 03:31:14 -0400

    Update MetadataType.cs
    Redefined the values as bit shifts instead of having to set the discrete values manually.
    Added some VtmV5 values for what has been defined so far.

--- OCSM/scripts/meta/MetadataType.cs
+++ OCSM/scripts/meta/MetadataType.cs
@@ -9,14 +9,19 @@ public enum MetadataType
 {
 	None = 0,
 	
-	CofdChangelingContractRegalia = 1,
-	CofdChangelingContractType = 2,
-	CofdChangelingCourt = 4,
-	CofdChangelingKith = 8,
-	CofdChangelingRegalia = 16,
-	CofdChangelingSeeming = 32,
+	CofdChangelingContractRegalia = 1 << 0,
+	CofdChangelingContractType = 1 << 1,
+	CofdChangelingCourt = 1 << 2,
+	CofdChangelingKith = 1 << 3,
+	CofdChangelingRegalia = 1 << 4,
+	CofdChangelingSeeming = 1 << 5,
 	
-	Dnd5eBackground = 64,
-	Dnd5eClass = 128,
-	Dnd5eSpecies = 256,
+	Dnd5eBackground = 1 << 6,
+	Dnd5eClass = 1 << 7,
+	Dnd5eSpecies = 1 << 8,
+	
+	WodVtmV5Advantage = 1 << 9,
+	WodVtmV5BloodResonance = 1 << 10,
+	WodVtmV5Flaw = 1 << 11,
+	WodVtmV5PredatorType = 1 << 12,
 }
