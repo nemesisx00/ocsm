be7659158ae4d46c0b05f568ca7f9598f8816a26
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2024-10-19 19:36:58 -0400

    Update OptionButtonExtensions.cs
    Syntax.

--- OCSM/scripts/util/OptionButtonExtensions.cs
+++ OCSM/scripts/util/OptionButtonExtensions.cs
@@ -10,32 +10,34 @@ public static class OptionButtonExtensions
 	public static void SetDisabled(this OptionButton node, List<string> strings, bool disabled = false, List<string> exclusions = null)
 	{
 		var skip = new List<string>();
+		
 		if(exclusions is not null)
 			skip.AddRange(exclusions);
 		
 		node.SetDisabledAll(false);
-		strings.ForEach(s => node.SetDisabledByText(s, disabled));
-		skip.ForEach(s => node.SetDisabledByText(s, !disabled));
+		
+		foreach(var s in strings)
+			node.SetDisabledByText(s, disabled);
+		
+		foreach(var s in skip)
+			node.SetDisabledByText(s, !disabled);
 	}
 	
 	public static void SetDisabledAll(this OptionButton node, bool disabled = false)
 	{
 		var count = node.ItemCount;
 		for(var i = 0; i < count; i++)
-		{
 			node.SetItemDisabled(i, disabled);
-		}
 	}
 	
 	public static void SetDisabledByText(this OptionButton node, string text, bool disabled = false)
-	{
-		node.SetItemDisabled(node.GetFirstItemIndexByText(text), disabled);
-	}
+		=> node.SetItemDisabled(node.GetFirstItemIndexByText(text), disabled);
 	
 	public static int GetFirstItemIndexByText(this OptionButton node, string text)
 	{
 		var index = -1;
 		var count = node.ItemCount;
+		
 		for(var i = 0; i < count; i++)
 		{
 			if(node.GetItemText(i).Equals(text))
@@ -44,6 +46,7 @@ public static class OptionButtonExtensions
 				break;
 			}
 		}
+		
 		return index;
 	}
 	
@@ -51,10 +54,13 @@ public static class OptionButtonExtensions
 	{
 		var ret = string.Empty;
 		var index = node.Selected;
+		
 		if(index >= 0 && index < node.ItemCount)
 			ret = node.GetItemText(index);
+		
 		return ret;
 	}
 	
-	public static void SelectItemByText(this OptionButton node, string text) => node.Selected = node.GetFirstItemIndexByText(text);
+	public static void SelectItemByText(this OptionButton node, string text)
+		=> node.Selected = node.GetFirstItemIndexByText(text);
 }
