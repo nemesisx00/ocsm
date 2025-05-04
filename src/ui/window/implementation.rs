98af149ef745b86090e2f59087f834ec49e9c555
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2025-05-04 16:35:12 -0400

    Update main.rs, implementation.rs, mod.rs, and 8 more files
    Updated to use Stack and StackSidebar for the main nav; Removed leftNav widget.
    Added a HomeScreen widget where the user will be able to create new sheets.
    Wired up a button on the HomeScreen to create a new StackPage using SheetCofdMortal.

--- src/ui/window/implementation.rs
+++ src/ui/window/implementation.rs
@@ -1,21 +1,61 @@
 use cofd::sheet::SheetCofdMortal;
-use gtk4::CompositeTemplate;
+use gtk4::glib::object::ObjectExt;
+use gtk4::{CompositeTemplate, Stack, StackSidebar, TemplateChild};
 use glib::subclass::InitializingObject;
-use gtk4::glib::{self};
+use gtk4::glib::{self, closure_local};
 use gtk4::glib::subclass::object::{ObjectImpl, ObjectImplExt};
 use gtk4::glib::subclass::types::{ObjectSubclass, ObjectSubclassExt};
 use gtk4::glib::types::StaticTypeExt;
 use gtk4::prelude::GtkWindowExt;
 use gtk4::subclass::prelude::ApplicationWindowImpl;
-use gtk4::subclass::widget::{CompositeTemplateClass, CompositeTemplateInitializingExt, WidgetImpl};
+use gtk4::subclass::widget::{CompositeTemplateClass, CompositeTemplateInitializingExt, WidgetClassExt, WidgetImpl};
 use gtk4::subclass::window::WindowImpl;
-use crate::ui::nav::LeftNav;
+use crate::ui::home::{HomeScreen, Signal_NewSheet};
 
 #[derive(CompositeTemplate, Default)]
 #[template(resource = "/io/github/nemesisx00/OCSM/window.ui")]
-pub struct OcsmWindow {}
+pub struct OcsmWindow
+{
+	#[template_child]
+	homeScreen: TemplateChild<HomeScreen>,
+	
+	#[template_child]
+	sidebar: TemplateChild<StackSidebar>,
+	
+	#[template_child]
+	stack: TemplateChild<Stack>,
+}
 
 impl ApplicationWindowImpl for OcsmWindow {}
+
+impl ObjectImpl for OcsmWindow
+{
+	fn constructed(&self)
+	{
+		self.parent_constructed();
+		
+		self.obj().set_default_size(640, 480);
+		
+		self.sidebar.set_stack(&self.stack);
+		
+		let me = self;
+		self.homeScreen.connect_closure(
+			Signal_NewSheet,
+			false,
+			closure_local!(#[weak] me, move |_: HomeScreen, gameSystem: u32| {
+				match gameSystem
+				{
+					0 => {
+						let sheet = SheetCofdMortal::new();
+						me.stack.add_titled(&sheet, Some("cofdMortal"), "Mortal");
+					},
+					
+					_ => {},
+				}
+			})
+		);
+	}
+}
 
 #[glib::object_subclass]
 impl ObjectSubclass for OcsmWindow
@@ -27,7 +67,7 @@ impl ObjectSubclass for OcsmWindow
 	
 	fn class_init(klass: &mut Self::Class)
 	{
-		LeftNav::ensure_type();
+		HomeScreen::ensure_type();
 		SheetCofdMortal::ensure_type();
 		klass.bind_template();
 	}
@@ -35,16 +75,6 @@ impl ObjectSubclass for OcsmWindow
 	fn instance_init(obj: &InitializingObject<Self>)
 	{
 		obj.init_template();
-	}
-}
-
-impl ObjectImpl for OcsmWindow
-{
-	fn constructed(&self)
-	{
-		self.parent_constructed();
-		
-		self.obj().set_default_size(640, 480);
 	}
 }
 
