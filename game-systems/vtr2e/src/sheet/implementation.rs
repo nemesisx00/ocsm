cf236645f460ba3a165f3dfab76b37f2304447ef
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2025-05-10 16:42:29 -0400

    Update implementation.rs and sheet.ui
    Updated sheet.ui so the existing widgets are Vampire themed.
    Wired up Blood Potency to affect max attribute/skill and max Vitae; Also, Stamina affects max Vitae when Blood Potency is 0.

--- game-systems/vtr2e/src/sheet/implementation.rs
+++ game-systems/vtr2e/src/sheet/implementation.rs
@@ -1,12 +1,13 @@
+use gtk4::glib::object::ObjectExt;
 use gtk4::glib::subclass::InitializingObject;
 use gtk4::glib::types::StaticTypeExt;
 use gtk4::prelude::WidgetExt;
 use gtk4::subclass::box_::BoxImpl;
-use gtk4::{Box, CompositeTemplate};
-use gtk4::glib::{self};
+use gtk4::{Box, CompositeTemplate, TemplateChild};
+use gtk4::glib::{self, closure_local};
 use gtk4::glib::subclass::types::{ObjectSubclass, ObjectSubclassExt};
 use gtk4::glib::subclass::object::{ObjectImpl, ObjectImplExt};
-use gtk4::subclass::widget::{CompositeTemplateClass, CompositeTemplateInitializingExt, WidgetImpl};
+use gtk4::subclass::widget::{CompositeTemplateClass, CompositeTemplateInitializingExt, WidgetClassExt, WidgetImpl};
 use widgets::statefultrack::StatefulTrack;
 use cofd::widgets::attributes::mental::AttributesCofdMental;
 use cofd::widgets::attributes::physical::AttributesCofdPhysical;
@@ -17,18 +18,69 @@ use cofd::widgets::skills::social::SkillsCofdSocial;
 use cofd::widgets::skills::SkillsCofd;
 
 #[derive(CompositeTemplate, Default)]
-//#[properties(wrapper_type = super::SheetCofdVtr2e)]
 #[template(resource = "/io/github/nemesisx00/OCSM/cofd/vtr2e/sheet.ui")]
-pub struct SheetCofdVtr2e {}
+pub struct SheetCofdVtr2e
+{
+	#[template_child]
+	attributesMental: TemplateChild<AttributesCofdMental>,
+	
+	#[template_child]
+	attributesPhysical: TemplateChild<AttributesCofdPhysical>,
+	
+	#[template_child]
+	attributesSocial: TemplateChild<AttributesCofdSocial>,
+	
+	#[template_child]
+	bloodPotencyTrack: TemplateChild<StatefulTrack>,
+	
+	#[template_child]
+	skillsMental: TemplateChild<SkillsCofdMental>,
+	
+	#[template_child]
+	skillsPhysical: TemplateChild<SkillsCofdPhysical>,
+	
+	#[template_child]
+	skillsSocial: TemplateChild<SkillsCofdSocial>,
+	
+	#[template_child]
+	vitaeTrack: TemplateChild<StatefulTrack>,
+}
 
 impl BoxImpl for SheetCofdVtr2e {}
 
-//#[glib::derived_properties]
 impl ObjectImpl for SheetCofdVtr2e
 {
 	fn constructed(&self)
 	{
 		self.parent_constructed();
+		
+		let rowLength = 5;
+		
+		self.attributesMental.setRowLength(rowLength);
+		self.attributesPhysical.setRowLength(rowLength);
+		self.attributesSocial.setRowLength(rowLength);
+		self.skillsMental.setRowLength(rowLength);
+		self.skillsPhysical.setRowLength(rowLength);
+		self.skillsSocial.setRowLength(rowLength);
+		
+		let me = self;
+		self.attributesPhysical.connectStamina(
+			StatefulTrack::Signal_ValueUpdated,
+			false,
+			closure_local!(
+				#[weak] me,
+				move |_: StatefulTrack, _: u32, _: u32, _: u32| me.handleBloodPotencyChanged()
+			)
+		);
+		
+		self.bloodPotencyTrack.connect_closure(
+			StatefulTrack::Signal_ValueUpdated,
+			false,
+			closure_local!(
+				#[weak] me,
+				move |_: StatefulTrack, _: u32, _: u32, _: u32| me.handleBloodPotencyChanged()
+			)
+		);
 	}
 	
 	fn dispose(&self)
@@ -68,4 +120,50 @@ impl ObjectSubclass for SheetCofdVtr2e
 
 impl WidgetImpl for SheetCofdVtr2e {}
 
-impl SheetCofdVtr2e {}
+impl SheetCofdVtr2e
+{
+	fn handleBloodPotencyChanged(&self)
+	{
+		let value = self.bloodPotencyTrack.get().value().one;
+		
+		let max = match value
+		{
+			6 => 6,
+			7 => 7,
+			8 => 8,
+			9 => 9,
+			10 => 10,
+			_ => 5,
+		};
+		
+		self.attributesMental.setMaximum(max);
+		self.attributesPhysical.setMaximum(max);
+		self.attributesSocial.setMaximum(max);
+		self.skillsMental.setMaximum(max);
+		self.skillsPhysical.setMaximum(max);
+		self.skillsSocial.setMaximum(max);
+		
+		let stamina = self.attributesPhysical.stamina();
+		
+		let glamourMax = match value
+		{
+			1 => 10,
+			2 => 11,
+			3 => 12,
+			4 => 13,
+			5 => 15,
+			6 => 20,
+			7 => 25,
+			8 => 30,
+			9 => 50,
+			10 => 75,
+			_ => match stamina
+			{
+				0 => 1,
+				_ => stamina,
+			},
+		};
+		
+		self.vitaeTrack.set_maximum(glamourMax);
+	}
+}
