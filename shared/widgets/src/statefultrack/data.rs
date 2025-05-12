5f0dd48e26b07824124fdd18005a611c5c471666
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2025-05-12 19:33:17 -0400

    Update data.rs
    Added a truncate method to easily truncate all three state values based on a given maximum.

--- shared/widgets/src/statefultrack/data.rs
+++ shared/widgets/src/statefultrack/data.rs
@@ -6,3 +6,25 @@ pub struct StateValue
 	pub two: u32,
 	pub three: u32,
 }
+
+impl StateValue
+{
+	pub fn truncate(&mut self, max: u32)
+	{
+		if self.three > max
+		{
+			self.three = max;
+			self.two = 0;
+			self.one = 0;
+		}
+		else if self.three + self.two > max
+		{
+			self.two = max - self.three;
+			self.one = 0;
+		}
+		else if self.three + self.two + self.one > max
+		{
+			self.one = max - self.three - self.two;
+		}
+	}
+}
